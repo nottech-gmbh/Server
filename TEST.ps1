e848114bdead9aae5e5cc46997c59a54abcf9fdf
nottech-gmbh <81640282+nottech-gmbh@users.noreply.github.com>
2021-08-19 16:35:05 +0200

    Update TEST.ps1

--- TEST.ps1
+++ TEST.ps1
@@ -1,6 +1,6 @@
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Script = Invoke-RestMethod -uri https://api.github.com/repos/nottech-gmbh/Client/contents/Get-Date.ps1?access_token=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 -Headers @{”Accept”= “application/vnd.github.v3.raw”}
+$Script = Invoke-RestMethod -uri https://api.github.com/repos/nottech-gmbh/Client/contents/Get-Date.ps1?access_token=xxxxxxxxxxxxxxxxxxxxxxxx -Headers @{”Accept”= “application/vnd.github.v3.raw”}
 
 Invoke-Expression $Script
 
